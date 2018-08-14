78121b54dc015a191232e04816d7cccc46d3183d
Ruben Bielen <ruben.bielen@gmail.com>
2018-08-14 13:36:58 +0200

    Remove boilerplate + build first stages of introduction

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,23 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        rubenbielen-frontend
-      </h1>
-      <h2 class="subtitle">
-        Portfolio frontend project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <section class="intro container">
+    <div class="row">
+      <div class="col-12 col-md-7 intro-caption">
+        <h1>Hi i'm <strong>Ruben</strong></h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 offset-md-5 col-md-7 intro-subtext">
+        <h3>And i really like <strong>webdevelopment</strong><span class="cursor">|</span></h3>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
-  }
-}
+  components: {}
+};
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style lang="scss" scoped>
 </style>
-
